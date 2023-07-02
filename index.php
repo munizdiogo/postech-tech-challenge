d56633ab77e92212d0763f2c1e9999a8dc634d45
munizdiogo <diogomuniz.web@gmail.com>
2023-07-02 01:06:10 -0300

    Implementação da classe e ajustes no index para validar Autenticação

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 require_once 'vendor/autoload.php';
+
+
 (new Controller\DotEnvEnvironment)->load(__DIR__);
 
+use Controller\AutenticacaoController;
 use Controller\ClienteController;
 use Controller\ProdutoController;
 use Controller\PedidoController;
@@ -13,6 +16,7 @@ use Infrastructure\Database;
 use Domain\Entities\ClienteDomain;
 use Domain\Entities\ProdutoDomain;
 use Domain\Entities\PedidoDomain;
+use Firebase\JWT\Key;
 
 $database = new Database();
 $clienteDomain = new ClienteDomain($database);
@@ -25,46 +29,102 @@ $clienteController = new ClienteController($clienteService, $clienteDomain);
 $produtoController = new ProdutoController($produtoService);
 $pedidoController = new PedidoController($pedidoService, $clienteService);
 
-if (!empty($_GET["acao"])) {
-    switch ($_GET["acao"]) {
-        case "cadastrarCliente":
-            $clienteController->cadastrar($_POST);
-            break;
+$autenticacaoController = new AutenticacaoController();
 
-        case "obterClientePorCPF":
-            $clienteController->buscarClientePorCPF($_GET["cpf"]);
-            break;
 
-        case "cadastrarProduto":
-            $produtoController->cadastrar($_POST);
-            break;
 
-        case "editarProduto":
-            $produtoController->editar($_POST);
-            break;
+$chaveSecreta = "teste";
 
-        case "excluirProduto":
-            $produtoController->excluir($_POST["id"]);
-            break;
 
-        case "obterProdutosPorCategoria":
-            $produtoController->obterProdutosPorCategoria($_GET["categoria"]);
-            break;
+if (isset($_GET['acao']) && $_GET['acao'] == 'gerar') {
+    if ($GLOBALS['chaveSecreta'] == $_POST['chaveSecreta']) {
+        echo $autenticacaoController->gerarTokenJWT('', $_POST['chaveSecreta']);
+    }
+} else {
+    $retornoValidacaoAcesso = validarAcesso();
+    $jsonRetornoValidacaoAcesso = json_decode($retornoValidacaoAcesso);
+  
 
-        case "cadastrarNovoPedido":
-            $jsonDados = file_get_contents("php://input");
-            $dados = json_decode($jsonDados, true);
-            $pedidoController->cadastrar($dados);
-            break;
+    if ($jsonRetornoValidacaoAcesso->status) {
 
-        case "obterPedidos":
-            $pedidoController->obterPedidos();
-            break;
+        if (!empty($_GET["acao"])) {
+            switch ($_GET["acao"]) {
+                case "cadastrarCliente":
+                    $clienteController->cadastrar($_POST);
+                    break;
 
-        default:
-            echo '{
-                    "mensagem": "A ação informada é inválida."
-                  }';
-            http_response_code(400);
+                case "obterClientePorCPF":
+                    $clienteController->buscarClientePorCPF($_GET["cpf"]);
+                    break;
+
+                case "cadastrarProduto":
+                    $produtoController->cadastrar($_POST);
+                    break;
+
+                case "editarProduto":
+                    $produtoController->editar($_POST);
+                    break;
+
+                case "excluirProduto":
+                    $produtoController->excluir($_POST["id"]);
+                    break;
+
+                case "obterProdutosPorCategoria":
+                    $produtoController->obterProdutosPorCategoria($_GET["categoria"]);
+                    break;
+
+                case "cadastrarNovoPedido":
+                    $jsonDados = file_get_contents("php://input");
+                    $dados = json_decode($jsonDados, true);
+                    $pedidoController->cadastrar($dados);
+                    break;
+
+                case "obterPedidos":
+                    $pedidoController->obterPedidos();
+                    break;
+
+                default:
+                    echo '{
+                        "mensagem": "A ação informada é inválida."
+                      }';
+                    http_response_code(400);
+            }
+        }
+    } else {
+        echo $retornoValidacaoAcesso;
+    }
+}
+
+
+
+
+function validarAcesso()
+{
+    http_response_code(401);
+
+    $headers = getallheaders();
+    $token = isset($headers['Authorization']) ? $headers['Authorization'] : null;
+
+    if ($token) {
+        $autenticacaoController = new AutenticacaoController();
+        $retorno = $autenticacaoController->validarTokenJWT($headers['Authorization'], new Key($GLOBALS['chaveSecreta'], 'HS256'));
+        $dados = json_encode($retorno);
+        $dadosUsuario = json_decode($dados, true);
+
+        if ($dadosUsuario) {
+            if ($dadosUsuario == 'Expired token') {
+                return json_encode(array('status' => false, 'mensagem' => 'Token expirado'));
+            } else {
+                if (http_response_code() == 200) {
+                    return json_encode(array('status' => true, 'mensagem' => $dadosUsuario));
+                } else {
+                    return json_encode(array('status' => false, 'mensagem' => 'Token nao encontrado'));
+                }
+            }
+        } else {
+            return json_encode(array('status' => false, 'mensagem' => 'Token nao encontrado'));
+        }
+    } else {
+        return json_encode(array('status' => false, 'mensagem' => 'Token nao encontrado'));
     }
 }
---
+++ src/Controller/AutenticacaoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Controller;
+
+use Exception;
+use Firebase\JWT\JWT;
+
+class AutenticacaoController
+{
+
+    function pegarHeaders()
+    {
+        $headers = array();
+        foreach ($_SERVER as $key => $value) {
+            if (substr($key, 0, 5) <> 'HTTP_') {
+                continue;
+            }
+            $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+            $headers[$key] = $value;
+        }
+        return $headers;
+    }
+
+
+
+    // Função para verificar o token JWT
+    function gerarTokenJWT($token = '', $chaveSecreta = '')
+    {
+        // Chave secreta para assinatura do token (deve ser mantida em segurança)
+        $chaveSecreta = $_ENV['CHAVE_SECRETA'];
+
+        // Dados do usuário
+        $dadosUsuario = array(
+            'id' => 1,
+            'nome' => 'Lanchonete XPTO'
+        );
+
+        if (!empty($chaveSecreta)) {
+
+            // Configurações do token
+            $tokenConfig = array(
+                'iss' => 'localhost',  // Emissor do token
+                'aud' => 'localhost',   // Audiência do token
+                'iat' => time(),                  // Data de emissão do token
+                'exp' => time() + (60 * 60 * 24),      // Data de expiração do token (1 hora)
+                'data' => $dadosUsuario           // Dados do usuário
+            );
+
+            $algoritimo = 'HS256';
+
+            // Gerar o token JWT
+            return $token = JWT::encode($tokenConfig, $chaveSecreta, $algoritimo);
+        }
+    }
+
+
+
+    // Função para verificar o token JWT
+    function validarTokenJWT($token, $chaveSecreta)
+    {
+        try {
+            $decoded = JWT::decode($token, $chaveSecreta);
+            http_response_code(200);
+            return (array) $decoded->data; // Retorna os dados do usuário contidos no token
+        } catch (Exception $e) {
+            http_response_code(401);
+            return $e->getMessage(); // Token inválido ou expirado
+        }
+    }
+}
